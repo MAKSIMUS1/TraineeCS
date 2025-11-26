24be43466728c5f0498d4e703aee3e4a0a6a5e14
Maksim <maksimus1132@gmail.com>
2025-11-26 14:49:23 +0300

    task #6

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace TemperatureConverter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string temperatureConverterDllName = "TemperatureConverterLib.dll";
+            string dllPath = Path.Combine(AppContext.BaseDirectory, temperatureConverterDllName);
+            Assembly asm = null;
+            try
+            {
+                if (!File.Exists(dllPath))
+                {
+                    Console.WriteLine($"Ошибка: DLL не существует по пути: {dllPath}");
+                    return;
+                }
+                asm = Assembly.LoadFrom(dllPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка загрузки DLL: {ex.Message}");
+                return;
+            }
+            ReflectionInfo.GetReflectionInformation(asm);
+
+            Type converterType = asm.GetType("TemperatureConverterLib.TemperatureConverter");
+            if(converterType == null)
+            {
+                Console.WriteLine("Класс не найден");
+                return;
+            }
+
+            MethodInfo celsiusToFahrenheit = converterType.GetMethod("CelsiusToFahrenheit");
+
+            double celsiusTemperature = 25;
+            object result = celsiusToFahrenheit.Invoke(null, new object[] { celsiusTemperature });
+
+            Console.WriteLine($"{celsiusTemperature}C = {result}F");
+        }
+    }
+}
---
+++ TemperatureConverter/ReflectionInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public static class ReflectionInfo
+    {
+        public static void GetReflectionInformation(Assembly assembly)
+        {
+            if(assembly == null)
+            {
+                Console.WriteLine("Сборка не загружена");
+                return;
+            }
+
+            Console.WriteLine($"Сборка: {assembly.FullName}");
+            foreach(var type in assembly.GetTypes())
+            {
+                Console.WriteLine($"Класс: {type.FullName}");
+                var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                foreach(var method in methods)
+                {
+                    string staticFlag = method.IsStatic ? "(static)" : "";
+                    Console.WriteLine($"Метод: {method.Name} {staticFlag}");
+                }
+
+                var properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                foreach (var prop in properties)
+                {
+                    Console.WriteLine($"Свойство: {prop.Name}");
+                }
+                
+                var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                foreach (var field in fields)
+                {
+                    Console.WriteLine($"Поле: {field.Name}");
+                }
+            }
+        }
+    }
+}
