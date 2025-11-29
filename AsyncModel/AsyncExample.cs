4cf24cd6d8a2e77dd145f2356eb303da14bf35c2
Maksim <maksimus1132@gmail.com>
2025-11-29 12:33:13 +0300

    Task #13

---
+++ AsyncModel/AsyncExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncModel
+{
+    public class AsyncExample
+    {
+        public string TestWork(string input)
+        {
+            Console.WriteLine("TestWork start");
+            Thread.Sleep(500);
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            return $"Processed '{input}' on thread {threadId}";
+        }
+
+        public void RunAsync(string input, Action<string> callback)
+        {
+            Task.Run(() =>
+            {
+                string result = TestWork(input);
+                return result;
+            })
+            .ContinueWith(task =>
+            {
+                callback(task.Result);
+            });
+        }
+
+        public static void Callback(string result)
+        {
+            Console.WriteLine("Callback received result:");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AsyncModel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using static AsyncModel.AsyncExample;
+
+namespace AsyncModel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AsyncExample ae = new AsyncExample();
+
+            ae.RunAsync("Hello", AsyncExample.Callback);
+
+            Console.WriteLine("Main thread continue");
+            Console.ReadLine();
+        }
+    }
+}
